LineageOS/android_bootable_recovery

recovery: Extend A/B downgrades for older SPL
This allows going back to stock ROM to progress

Change-Id: If8a3ab7347e51c90337c4044826226ea11d6a327

install/spl_check.cpp
@@ -30,7 +30,7 @@ bool ViolatesSPLDowngrade(const build::tools::releasetools::OtaMetadata& metadat
   // example, 05 for May). Otherwise this comparison doesn't work. We don't expect SPL date formats
   // to change, leave this as is.
   if (post_spl < current_spl) {
-    LOG(ERROR) << "Current SPL: " << current_spl << " Target SPL: " << post_spl
+    LOG(INFO) << "Current SPL: " << current_spl << " Target SPL: " << post_spl
                << " this is considered a downgrade";
     if (metadata.spl_downgrade() || metadata.downgrade()) {
       LOG(WARNING)

ProjectInfinity-X/bootable_recovery

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

install/spl_check.cpp
@@ -30,7 +30,7 @@ bool ViolatesSPLDowngrade(const build::tools::releasetools::OtaMetadata& metadat
   // example, 05 for May). Otherwise this comparison doesn't work. We don't expect SPL date formats
   // to change, leave this as is.
   if (post_spl < current_spl) {
-    LOG(ERROR) << "Current SPL: " << current_spl << " Target SPL: " << post_spl
+    LOG(WARNING) << "Current SPL: " << current_spl << " Target SPL: " << post_spl
                << " this is considered a downgrade";
     if (metadata.spl_downgrade() || metadata.downgrade()) {
       LOG(WARNING)